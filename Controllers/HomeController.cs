ff85f49253f662f85587a760e53ba039f1819bf1
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2024-05-22 16:03:09 +0530

    update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,236 @@
+using FINDMYAREA.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FINDMYAREA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index_1()
+        {
+            return View();
+        }
+        public IActionResult Add_listing()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Blog_Details_Left_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Blog_Details_Right_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Blog_Page()
+        {
+            return View();
+        }
+        public IActionResult Blog_Page_Left_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Blog_Page_Right_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_Add_Listing()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_Bookings()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_BookMark()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_Change_Password()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_Massages()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_My_Listing()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_My_Profile()
+        {
+                return View();
+        }
+        public IActionResult DashBoard_Submitted_Review()
+        {
+            return View();
+        }
+        public IActionResult DashBoard_Visitor_Review()
+        {
+            return View();
+        }
+        public IActionResult Dashboard_Wallet()
+        {
+            return View();
+        }
+        public IActionResult Index_2()
+        {
+            return View();
+        }
+        public IActionResult Index_3()
+        {
+            return View();
+        }
+        public IActionResult Index_4()
+        {
+            return View();
+        }
+        public IActionResult Index_5()
+        {
+            return View();
+        }
+        public IActionResult Index_6()
+        {
+            return View();
+        }
+        public IActionResult Index_7()
+        {
+            return View();
+        }
+        public IActionResult Listing_Booking()
+        {
+            return View();
+        }
+        public IActionResult Listing_Grid_Full_Width()
+        {
+            return View();
+        }
+        public IActionResult Listing_Grid_Full_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Listing_Half_Screen_Map_Grid()
+        {
+            return View();
+        }
+        public IActionResult Listing_Half_Screen_Map_List()
+        {
+            return View();
+        }
+        public IActionResult Listing_List_Full_Width()
+        {
+            return View();
+        }
+        public IActionResult Listing_List_With_Sidebar()
+        {
+            return View();
+        }
+        public IActionResult Listing_Single_Page_1()
+        {
+            return View();
+        }
+        public IActionResult Listing_Single_Page_2()
+        {
+            return View();
+        }
+        public IActionResult Listing_Single_Page_3()
+        {
+            return View();
+        }
+        public IActionResult Listing_Single_Page_4()
+        {
+            return View();
+        }
+        public IActionResult Listing_Single_Page_5()
+        {
+            return View();
+        }
+        public IActionResult Listing_Two_Column_Map_Grid()
+        {
+            return View();
+        }
+        public IActionResult Page_About()
+        {
+            return View();
+        }
+        public IActionResult Page_Categories_One()
+        {
+            return View();
+        }
+        public IActionResult Page_Categories_Two()
+        {
+            return View();
+        }
+        public IActionResult Page_Coming_Soon()
+        {
+            return View();
+        }
+        public IActionResult Page_Element()
+        {
+            return View();
+        }
+        public IActionResult Page_Faqs()
+        {
+            return View();
+        }
+        public IActionResult Page_Filtering_Style()
+        {
+            return View();
+        }
+        public IActionResult Page_Icons_One()
+        {
+            return View();
+        }
+        public IActionResult Page_Icons_Two()
+        {
+            return View();
+        }
+        public IActionResult Page_Icons_Three()
+        {
+            return View();
+        }
+        public IActionResult Page_Not_Found()
+        {
+            return View();
+        }
+        public IActionResult Page_Pricing()
+        {
+            return View();
+        }
+        public IActionResult Typography()
+        {
+            return View();
+        }
+
+        public IActionResult WishList_Listings()
+        {
+            return View();
+        }
+
+
+    }
+}
